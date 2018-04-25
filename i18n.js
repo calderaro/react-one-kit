c21e03525d6f852c104266896dfd77df633029b1
Angel calderaro <calderaro94@gmail.com>
2018-04-25 13:01:41 -0500

    fix i18n

--- i18n.js
+++ i18n.js
@@ -2,8 +2,8 @@ const i18n = require('i18n')
 const reacti18next = require('react-i18next')
 
 module.exports = {
-  i18n,
-  reactI18nextModule: reacti18next.reactI18nextModule
-  I18nextProvider: reacti18next.I18nextProvider,
-  translate: I18nextProvider.translate
+  'i18n': i18n,
+  'reactI18nextModule': reacti18next.reactI18nextModule
+  'I18nextProvider': reacti18next.I18nextProvider,
+  'translate': reacti18next.translate
 }
